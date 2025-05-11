84623ffb244ee09585329b4da2ea2ec84bcc8f38
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2025-05-12 01:21:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on ${process.env.PORT}`);
 });
